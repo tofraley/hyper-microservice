a69d35d12fc1b8a4d86c715f2282738a88f0e415
Taylor Fraley <tofraley@gmail.com>
2019-10-26 14:25:57 -0400

    add basic routing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
-use hyper::{Body, Response, Server};
-use hyper::rt::Future;
-use hyper::service::service_fn_ok;
+use futures::{future, Future};
+use hyper::{Body, Error, Method, Request, Response, Server, StatusCode};
+use hyper::service::service_fn;
 
 fn main() {
+    const INDEX: &'static str = r#"
+    <!doctype html>
+    <html>
+        <head>
+            <title>Rust Microservice</title>
+        </head>
+        <body>
+            <h3>Rust Microservice</h3>
+        </body>
+    </html>
+    "#;
     let addr = ([127, 0, 0, 1], 8080).into();
     let builder = Server::bind(&addr);
-    let server = builder.serve(|| {
-        service_fn_ok(|_| {
-            Response::new(Body::from("Almost microservice..."))
-        })
-    });
+    let server = builder.serve(|| service_fn(microservice_handler));
+    fn microservice_handler(req: Request<Body>)
+        -> impl Future<Item=Response<Body>, Error=Error>
+    {
+        match (req.method(), req.uri().path()) {
+            (&Method::GET, "/") => {
+                future::ok(Response::new(INDEX.into()))
+            },
+            _ => {
+                let response = Response::builder()
+                    .status(StatusCode::NOT_FOUND)
+                    .body(Body::empty())
+                    .unwrap();
+                future::ok(response)
+            },
+        }
+    }
     let server = server.map_err(drop);
     println!("Starting server at http://{}", addr);
     hyper::rt::run(server);
